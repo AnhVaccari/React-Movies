c16e98b98abd605bfaa8e1c8fe4acbbc23b18193
Anna Va <annava8899@gmail.com>
2022-03-24 11:53:42 +0100

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Form from '../components/Form';
+import Header from '../components/Header';
+
+const Home = () => {
+  return (
+    <div>
+      <div className="home-page"></div>
+      <Header />
+      <Form />
+    </div>
+  );
+};
+
+export default Home;
